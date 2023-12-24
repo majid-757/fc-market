4ea26dda684f20d4c9ae84f2f9cf333e8409a46f
unknown <majid.asadollahi95@gmail.com>
2023-12-24 16:19:27 +0330

    General changes

---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
+
+// import products from '../products'
+import Product from '../components/Product/Product'
+
+function Home() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await axios.get('http://localhost:8000/api/products/')
+
+      setProducts(response.data)
+    }
+
+    sendRequest()
+  }, [])
+
+  return (
+    <div>
+      <h1>محصولات</h1>
+      <Row>
+        {products.map((item) => {
+            return (
+                <Col key={item._id} sm={12} md={6} lg={4}>
+                    <Product product={item} />
+                </Col>
+            )
+        })}
+      </Row>
+    </div>
+  )
+}
+
+export default Home
+
+
