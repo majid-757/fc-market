53ca2600e222233dc426aaa16ed9d2696e450418
unknown <majid.asadollahi95@gmail.com>
2023-12-24 22:05:47 +0330

    add redux and config action

---
+++ src/action/productAction.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+export const productListActions = () => async (dispatch) => {
+    try {
+        dispatch({ type: 'PRODUCT_LIST_REQUEST' })
+
+        const { data } = await axios.get('http://localhost:8000/api/products')
+
+        dispatch({
+            type: 'PRODUCT_LIST_SUCCESS',
+            payload: data,
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
